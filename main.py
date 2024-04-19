08c40bba5b1493f160fe9ed4ec786163430049ba
jamesy <jyab0617@gmail.com>
2024-04-19 00:04:06 -0400

    implement recording to sqlite database

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sqlite3
 import requests
 import time
 from datetime import date
@@ -15,6 +16,19 @@ soup = BeautifulSoup(response.content, "html.parser")
 # time.sleep(5)
 # Find the table by its ID
 table = soup.find("table", {"id": "main_table_countries_today"})
+
+# connect to the sqlite database
+conn = sqlite3.connect('coronavirus.db')
+c = conn.cursor()
+
+# Create a table if it doesn't exist
+# ISO 8601 date format: YYYY-MM-DD HH:MM:SS
+c.execute('''CREATE TABLE IF NOT EXISTS coronavirus_data
+             (country TEXT, total_cases INTEGER, new_cases INTEGER, 
+             total_deaths INTEGER, new_deaths INTEGER, total_recovered INTEGER, 
+             new_recovered INTEGER, active_cases INTEGER, serious_critical INTEGER,
+             total_cases_per_million REAL, deaths_per_million REAL, total_tests INTEGER,
+             tests_per_million REAL, population INTEGER, date TEXT)''')
 
 # Iterate over the rows of the table
 for row in table.find_all("tr"):
@@ -43,8 +57,20 @@ for row in table.find_all("tr"):
         total_tests = cells[12].text.strip()
         tests_per_million = cells[13].text.strip()
         population = cells[14].text.strip()
+
+        # get current date in ISO 8601 format
         date = date.today()
+        date_format = date.strftime("%Y-%m-%d %H:%M:%S")
+
+        insert_statement = "INSERT INTO coronavirus_data VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        data = (country, total_cases, new_cases, total_deaths, new_deaths, total_recovered, new_recovered,
+                active_cases, serious_critical, total_cases_per_million, deaths_per_million, total_tests,
+                tests_per_million, population, date_format)
+        c.execute(insert_statement, data)
 
         # Print or process the extracted data as needed
         # if country != "Total:":
-        print(f"Country: {country}, Total Cases: {total_cases}, New Cases: {new_cases}")
+        # print(f"Country: {country}, Total Cases: {total_cases}, New Cases: {new_cases}")
+
+conn.commit()
+conn.close()
